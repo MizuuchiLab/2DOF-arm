97b6997ff782d0133ea6a6f9ef956022a25c387e
Ikuo Mizuuchi <ikuo@mizuuchi.lab.tuat.ac.jp>
2024-10-22 19:22:56 +0900

    SampleServoCiecle2DOFarm.ino 2023/10/26 version

---
+++ SampleServoCircle2DOFarm.ino
@@ -0,0 +1,52 @@
+#include <Servo.h>
+Servo myservo1, myservo2;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  myservo1.attach( 9, 920, 2120);
+  myservo2.attach(10, 920, 2120);
+  myservo1.write(90);
+  myservo2.write(90);
+  delay(1000);
+}
+
+long TMAX=(50);
+#define L1  (80)  // Length of the link1 [mm]
+#define L2  (60)  // Length of the link2 [mm]
+#define XC  (110) // Center of the circle [mm]
+#define YC  (0)   // Center of the circle [mm]
+#define R   (15)  // Radius of the circle [mm]
+#define RAD2DEG(RAD)  (180*RAD/PI)
+
+void loop() {
+  float rad, xe, ye, theta1, theta2;
+  int theta1deg, theta2deg;
+  Serial.println("Begin:");
+  // degmin ~ degmid (y=a*t^2)
+  for (int t = 0; t < TMAX; t++) {
+    rad = (2*PI) * t / TMAX;
+    xe = XC + R * cos(rad);
+    ye = YC + R * sin(rad);
+    theta1=calc_theta1(xe, ye);
+    theta2=calc_theta2(xe, ye);
+    theta1deg=RAD2DEG(theta1); theta2deg=RAD2DEG(theta2);
+//    myservo1.write(theta1deg));
+//    myservo2.write(theta2deg);
+    Serial.print(theta1deg); Serial.print(", "); Serial.println(theta2deg);
+    delay(20);
+  }
+  Serial.println("End");
+}
+
+float calc_theta1 (float xe, float ye) {
+  float theta1;
+  // calculating theta1 using xe and ye
+  return theta1;
+}
+
+float calc_theta2 (float xe, float ye) {
+  float theta2;
+  // calculating theta2 using xe and ye
+  return theta2;
+}
